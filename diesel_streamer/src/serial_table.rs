4ed0a7f516fd951ee8e45da436fabc968aeb5d01
Joshua Oladele <jurshsmith@gmail.com>
2023-07-16 15:14:20 +0100

    Add sync and async feature flags

--- diesel_streamer/src/serial_table.rs
+++ diesel_streamer/src/serial_table.rs
@@ -1,13 +1,13 @@
 // TODO: Update macro to be more composable for defaults
 
-/// Streams a serial table for diesel schema. A serial table has an
-/// autoincremented field which allows this util cursor through the
-/// table for processing.
+/// Streams a serial table for diesel schemas. A serial table has an
+/// autoincremented field which is used to cursor through the table
+/// for processing.
 ///
 ///
 /// # Examples
 ///
-/// Stream serial table with default chunk_size, from, and to
+/// Stream a serial table with default chunk_size, from, and to
 ///
 /// ```ignore
 /// async fn main() {
@@ -74,33 +74,65 @@ macro_rules! stream_serial_table {
         )
     }};
 
-    ( $query:expr ,   $cursor_field:expr ,  $conn: expr ,  $chunk_size:expr , $from:expr, $to:expr, $stream_processor: expr,) => {{
-        use diesel::dsl::max;
-        use diesel::QueryDsl;
+    ( $query:expr ,   $cursor_field:expr ,  $conn: expr ,  $chunk_size:expr , $from:expr, $to:expr, $stream_processor: expr,) => {
+        #[cfg(feature = "async")]
+        {
+            use diesel::dsl::max;
+            use diesel::QueryDsl;
 
-        let to = match $to {
-            Some(to) => to,
-            None => $query
-                .select(max($cursor_field))
-                .get_result::<Option<i32>>(&mut $conn)
-                .await
-                .unwrap()
-                .unwrap(),
-        };
+            let to = match $to {
+                Some(to) => to,
+                None => $query
+                    .select(max($cursor_field))
+                    .get_result::<Option<i32>>(&mut $conn)
+                    .await
+                    .unwrap()
+                    .unwrap(),
+            };
 
-        while $from <= to {
-            let streamed_data = $query
-                .filter($cursor_field.eq_any($from..=to))
-                .load(&mut $conn)
-                .await
-                .unwrap();
+            while $from <= to {
+                let streamed_data = $query
+                    .filter($cursor_field.eq_any($from..=to))
+                    .load(&mut $conn)
+                    .await
+                    .unwrap();
 
-            // Because we move the streamed_data into the processor directly,
-            // it should go out of scope thereby resulting to
-            // freed memory after Rust auto-calls DROP function for us
-            ($stream_processor)(streamed_data).await;
+                // Since streamed_data is MOVED into the processor directly,
+                // it should go out of scope thereby resulting to
+                // freed memory right after Rust auto-calls DROP function
+                ($stream_processor)(streamed_data).await;
 
-            $from = $from + $chunk_size;
+                $from = $from + $chunk_size;
+            }
         }
-    }};
+
+        #[cfg(feature = "sync")]
+        {
+            use diesel::dsl::max;
+            use diesel::QueryDsl;
+
+            let to = match $to {
+                Some(to) => to,
+                None => $query
+                    .select(max($cursor_field))
+                    .get_result::<Option<i32>>(&mut $conn)
+                    .unwrap()
+                    .unwrap(),
+            };
+
+            while $from <= to {
+                let streamed_data = $query
+                    .filter($cursor_field.eq_any($from..=to))
+                    .load(&mut $conn)
+                    .unwrap();
+
+                // Since streamed_data is MOVED into the processor directly,
+                // it should go out of scope thereby resulting to
+                // freed memory right after Rust auto-calls DROP function
+                ($stream_processor)(streamed_data);
+
+                $from = $from + $chunk_size;
+            }
+        }
+    };
 }
