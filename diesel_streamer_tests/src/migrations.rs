394fdd8f6718530f29c52c238d6d36eab6426e7e
Joshua Oladele <jurshsmith@gmail.com>
2023-07-17 16:16:51 +0100

    Introduce Migrations
    This will house all test migrations.
    Written in raw SQL for now.

---
+++ diesel_streamer_tests/src/migrations.rs
@@ -0,0 +1,24 @@
+use diesel::{pg::PgConnection, sql_query, RunQueryDsl};
+
+pub fn run(conn: &mut PgConnection) {
+    create_user_table(conn);
+}
+
+fn create_user_table(conn: &mut PgConnection) {
+    // sql_query("DROP TABLE IF EXISTS users")
+    //     .execute(conn)
+    //     .unwrap();
+
+    sql_query(
+        r#"
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+        )
+    "#,
+    )
+    .execute(conn)
+    .unwrap();
+}
