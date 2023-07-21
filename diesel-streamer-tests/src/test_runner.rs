6192e6459b5e003d8428e9c4cbaa9e12683ada42
Joshua Oladele <jurshsmith@gmail.com>
2023-07-21 23:56:43 +0100

    Allow skipping db setup in test runner

--- diesel-streamer-tests/src/test_runner.rs
+++ diesel-streamer-tests/src/test_runner.rs
@@ -1,5 +1,8 @@
+use std::env;
+
 use diesel::prelude::*;
 use diesel::PgConnection;
+use dotenvy::dotenv;
 
 use crate::db;
 
@@ -7,11 +10,20 @@ pub fn run_test<TestFn>(test_fn: TestFn)
 where
     TestFn: FnOnce(&mut PgConnection),
 {
-    let mut conn = db::establish_connection();
+    let mut conn = match should_skip_db_setup() {
+        true => db::establish_connection(),
+        false => db::setup(),
+    };
 
     conn.test_transaction(|conn| -> Result<(), ()> {
         test_fn(conn);
 
         Ok(())
     });
+}
+
+fn should_skip_db_setup() -> bool {
+    dotenv().ok();
+
+    env::var("SKIP_DB_SETUP").is_ok()
 }
