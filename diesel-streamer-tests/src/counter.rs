5db664957773155287a3eda444b2ea51235f31f3
Joshua Oladele <jurshsmith@gmail.com>
2023-07-22 00:50:10 +0100

    Fix pedantic clippy lints

--- diesel-streamer-tests/src/counter.rs
+++ diesel-streamer-tests/src/counter.rs
@@ -3,21 +3,18 @@ pub struct Counter {
 }
 
 impl Counter {
+    #[must_use]
     pub fn new(initial_value: u16) -> Self {
         Self {
             value: Box::new(initial_value),
         }
     }
 
-    pub fn increment(&self) -> Self {
-        Self {
-            value: Box::new(*self.value + 1),
-        }
+    pub fn increment(&mut self) {
+        *self.value += 1;
     }
 
-    pub fn decrement(&self) -> Self {
-        Self {
-            value: Box::new(std::cmp::max(0, *self.value - 1)),
-        }
+    pub fn decrement(&mut self) {
+        *self.value = std::cmp::max(0, *self.value - 1);
     }
 }
--- diesel-streamer-tests/src/main.rs
+++ diesel-streamer-tests/src/main.rs
@@ -3,5 +3,5 @@ use diesel_streamer_tests::db;
 fn main() {
     // Run once to setup database
     // Useful in a CI environment running parallel tests
-    db::setup();
+    let _connection_after_setup = db::setup();
 }
